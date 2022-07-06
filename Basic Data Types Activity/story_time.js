9faa9ebc750afe950173c34cffd5bdc175923cd5
emmylo1 <108785218+emmylo1@users.noreply.github.com>
2022-07-06 08:36:01 -0700

    Add files via upload

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,29 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Welcome to my kitchen!";
+let intro = "My name is Emmy.";
+let setting = "We're in my house in Los Angeles.";
+let action = "Today, I'm baking cookies.";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 35;
+let year = 2040;
+let century = 21;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+let items = ["flour", "sugar", "butter", "chocolate chips", "baking soda", "eggs"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+let ovenOn = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + " " + intro + " " + setting);
+console.log("I'm " + age + ", and we're in the " + century +"st century. The year is " + year + ".");
+console.log(action + " I've mixed all the ingredients into a dough: " + items + ". Now it's time to bake!");
+console.log("My cookies are done, and it's " + ovenOn + ". They're delicious!");
